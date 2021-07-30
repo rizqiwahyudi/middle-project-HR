003dbd20dd27434ad970f93816127d157702bab8
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-07-30 16:59:16 +0700

    Update Dependencies & Update config/database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Illuminate\Database\DBAL\TimestampType;
 
 return [
 
@@ -142,6 +143,12 @@ return [
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
+    ],
+
+    'dbal' => [
+        'types' => [
+            'timestamp' => TimestampType::class,
+        ],
     ],
 
 ];
